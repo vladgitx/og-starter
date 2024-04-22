84869e9bd5eedbfe03863e9635addad677b686b1
vladgitx <vladd.balea@gmail.com>
2024-04-22 13:50:34 +0300

    Add OG

--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,6 @@
-console.log("Hello World!")
+import { og } from "open-godfather"
+
+og.events.on("playerConnect", (player) => {
+    player.spawn(new og.Vector3(0, 0, 3))
+    player.sendMessage("Hello from Open Godfather!")
+})
