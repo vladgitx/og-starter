9e0a02f647318379e7917ccb80982109a8aa183b
Vlad <108018370+vladgitx@users.noreply.github.com>
2024-04-30 23:07:58 +0300

    Update index.ts

--- src/index.ts
+++ src/index.ts
@@ -9,34 +9,33 @@ og.events.on("init", () => {
 })
 
 og.events.on("playerConnect", async (player) => {
-    const response = await player.dialog.show.list("Wanna spawn?", ["Los Santos", "Las Venturas", "San Fiero"], "Select", "Leave")
-
-    if (!response) {
-        // Player disconnected or a new dialog popped up
-        return
-    }
-
-    if (!response.action) {
-        // Player clicked on "Leave"
-        player.kick()
-        return
-    }
-
     const SPAWNS = [
         { position: new og.Vector3(2481.1885, -1536.7186, 24.1467), rotation: 273.4944 }, // Los Santos
         { position: new og.Vector3(2087.9902, 1516.5336, 10.8203), rotation: 48.93 }, // Las Venturas
         { position: new og.Vector3(-2287.5027, 149.1875, 35.3125), rotation: 266.3989 }, // San Fiero
     ]
-
-    player.spawn(SPAWNS[response.item].position, SPAWNS[response.item].rotation)
-
-    player.skin = 211
-    player.cash = 450
-
-    player.weapons.add(WeaponsEnum.Colt45, 99999)
-    player.weapons.setSkill(WeaponSkillsEnum.Colt45, 1)
-
-    player.sendMessage("Welcome to Open Godfather! Type /vx to spawn a vehicle.", "a069c7")
+    
+    try {
+        const response = await player.dialog.show.list("Wanna spawn?", ["Los Santos", "Las Venturas", "San Fiero"], "Select", "Leave")
+    
+        if (!response.action) {
+            // Player clicked on "Leave"
+            player.kick()
+            return
+        }
+    
+        player.spawn(SPAWNS[response.item].position, SPAWNS[response.item].rotation)
+    
+        player.skin = 211
+        player.cash = 450
+    
+        player.weapons.add(WeaponsEnum.Colt45, 99999)
+        player.weapons.setSkill(WeaponSkillsEnum.Colt45, 1)
+    
+        player.sendMessage("Welcome to Open Godfather! Type /vx to spawn a vehicle.", "a069c7")
+    } catch (error) {
+        // Player disconnected or a new dialog popped up
+    }
 })
 
 og.commands.add("/vx", [], (player, modelParam) => {
